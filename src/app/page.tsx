01111ad9a1a180cf4f4512a2f307dd8e5bf7e124
Suheab Khan <66318470+suheabkhan@users.noreply.github.com>
2024-06-18 18:25:57 +0530

    Add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Welcome to elib client app</h1>;
+  return (
+    //Here We Need To Use This NavBar For Every Page So This May Cause To Duplication Of Code
+    //We Need To Avoid This
+    //By Using The Feature Of Layouts In Next Js We Can Remove Duplication
+    //So Paste It Above Children Component Of Body Part
+    <>
+      <h1>Welcome to elib client app</h1>
+    </>
+  );
 }
